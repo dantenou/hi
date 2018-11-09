a31e20f2ff085fd98936f5539c074b8692906544
litaoran <songhexiaochangyin@163.com>
2018-11-09 15:15:07 +0800

    改名后的工程

---
+++ Database/src/engine_interface/Sort.java
@@ -0,0 +1,9 @@
+package engine_interface;
+import datastructure.*;
+
+public interface Sort {
+	//实现排序运算的接口,继承类名规定为Sorter,Sorter用组合方式定义为执行引擎中的一个内部类
+	
+	public BTreeSet_interface sort(BTreeSet_interface bTreeSet,int... index);
+	//参数中的index表明排序依据的一个或多个类
+}
