0540a66e6fc0933e1503fa11f335c5ddfdf5dca4
yds <yds615@outlook.com>
2018-11-09 16:07:33 +0800

    假b树

---
+++ Database/src/engine/Connector.java
@@ -0,0 +1,30 @@
+package engine;
+import java.util.ArrayList;
+
+import datastructure.BTreeSet_interface;
+import datastructure.Node_interface;
+import engine_interface.*;
+import datastructure.*;
+public class Connector implements Connect{
+
+	@Override
+	public BTreeSet_interface cartesian(BTreeSet_interface bTreeSet1, BTreeSet_interface bTreeSet2) {
+		BTreeSet_interface bt=new BTreeSet();
+		
+		return null;
+	}
+
+	@Override
+	public BTreeSet_interface natural(BTreeSet_interface bTreeSet1, BTreeSet_interface bTreeSet2) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public BTreeSet_interface outer(BTreeSet_interface bTreeSet1, BTreeSet_interface bTreeSet2, boolean full,
+			boolean left) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+}
